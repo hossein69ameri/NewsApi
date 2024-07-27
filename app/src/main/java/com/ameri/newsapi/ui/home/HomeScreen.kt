176877cb40bdf94740fb83d092a824aa856ceb6e
hossein <hossein.smilinno@gmail.com>
2024-07-27 08:07:26 +0330

    Delete Ui for Test Api

--- app/src/main/java/com/ameri/newsapi/ui/home/HomeScreen.kt
+++ app/src/main/java/com/ameri/newsapi/ui/home/HomeScreen.kt
@@ -1,32 +1,12 @@
 package com.ameri.newsapi.ui.home
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
-import com.ameri.newsapi.util.theme.AntiFlashWhite
 import com.ameri.newsapi.viewmodel.HomeViewModel
 
 @Composable
 fun HomeScreen(navController: NavHostController,viewModel: HomeViewModel = hiltViewModel()) {
 
-    Box(
-        modifier = Modifier
-            .background(AntiFlashWhite)
-            .fillMaxSize(),
-        contentAlignment = Alignment.Center
-    ){
-        Button(onClick = {
-            viewModel.getTopHeadlinesData()
-        }){
-            Text(text = "apicall")
-        }
-    }
+
 }

73626b5291e6f0cc35b0da58d6323f542bc3d16d
hossein <hossein.smilinno@gmail.com>
2024-09-24 14:19:15 +0330

    Add Compose Compiler Plugin in Each Module For Fix Compile Error

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,5 +6,4 @@ plugins {
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.compose.compiler) apply false
     alias(libs.plugins.android.library) apply false
-
 }
--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.compose.compiler)
+
 }
 
 android {
